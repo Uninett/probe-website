e6611274dd8b00e12f07aea8f7cd34d85c876650
jakobsn <jakob_sn_@hotmail.com>
2017-07-26 11:52:09 +0200

    added shutdown script for any compatible adapter to raspbian init script

--- image_generation/probe_init_raspbian.sh
+++ image_generation/probe_init_raspbian.sh
@@ -80,7 +80,7 @@ cp "/root/init/host_key" "/root/.ssh/known_hosts"
 echo "[+] Gathering wlan MAC address"
 mac=$(ifconfig wlan0 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
 # The test checks wether this is the internal wireless card
-if [[ "${mac}" == "" || test -e /sys/class/net/wlan0/phy80211/device/driver/module/drivers/usb\:brcmfmac ]]; then
+if [[ "${mac}" == "" || -e /sys/class/net/wlan0/phy80211/device/driver/module/drivers/usb\:brcmfmac ]]; then
     mac=$(ifconfig wlan1 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
     if [[ "${mac}" == "" ]]; then
         log_error "Unable to read wlan MAC"
@@ -160,6 +160,14 @@ EOF
 systemctl enable reverse_ssh
 
 echo "[+] Add shutdown script for the wifi adapter"
+adapter=$(lsusb | grep -i '2357:010C\|056E:4008\|2001:3311\|0DF6:0076\|2001:3310\|2001:330F\|07B8:8179\|0BDA:0179\|0BDA:8179\|0411:025D\|2019:AB32\|7392:A813\|056E:4007\|0411:0242\|0846:9052\|056E:400F\|056E:400E\|0E66:0023\|2001:3318\|2001:3314\|04BB:0953\|7392:A812\|7392:A811\|0BDA:0823\|0BDA:0820\|0BDA:A811\|0BDA:8822\|0BDA:0821\|0BDA:0811\|2357:0122\|148F:9097\|20F4:805B\|050D:1109\|2357:010D\|2357:0103\|2357:0101\|13B1:003F\|2001:3316\|2001:3315\|07B8:8812\|2019:AB30\|1740:0100\|1058:0632\|2001:3313\|0586:3426\|0E66:0022\|0B05:17D2\|0409:0408\|0789:016E\|04BB:0952\|0DF6:0074\|7392:A822\|2001:330E\|050D:1106\|0BDA:881C\|0BDA:881B\|0BDA:881A\|0BDA:8812\|2357:0109\|2357:0108\|2357:0107\|2001:3319\|0BDA:818C\|0BDA:818B\|148F:7650\|0B05:17D3\|0E8D:760A\|0E8D:760B\|13D3:3431\|13D3:3434\|148F:6370\|148F:7601\|148F:760A\|148F:760B\|148F:760C\|148F:760D\|2001:3D04\|2717:4106\|2955:0001\|2955:1001\|2955:1003\|2A5F:1000\|7392:7710\|0E8D:7650\|0E8D:7630\|2357:0105\|0DF6:0079\|0BDB:1011\|7392:C711\|20F4:806B\|293C:5702\|057C:8502\|04BB:0951\|07B8:7610\|0586:3425\|2001:3D02\|2019:AB31\|0DF6:0075\|0B05:17DB\|0B05:17D1\|148F:760A\|148F:761A\|7392:B711\|7392:A711\|0E8D:7610\|13B1:003E\|148F:7610\|0E8D:7662\|0E8D:7632\|0B05:17C9\|0E8D:7612\|045E:02E6\|0B05:17EB\|0846:9053\|0B05:180B\|0846:9014')
+if [[ $adapter ]]; then
+    id=$(echo $adapter | awk '{ print$6 }')
+    ID_VENDOR_ID=$(echo $id | cut -d ':' -f 1)
+    ID_MODEL_ID=$(echo $id | cut -d ':' -f 2)
+    sudo echo 'ACTION=="remove", ENV{ID_VENDOR_ID}=="'"${ID_VENDOR_ID}"'", ENV{ID_MODEL_ID}=="'"${ID_MODEL_ID}"'", RUN+="/sbin/shutdown -h now"' > /etc/udev/rules.d/00-dongle_shutdown.rules
+fi
+
 
 # Prevent this script from running on subsequent boots
 echo "[+] Disabling this init script"
